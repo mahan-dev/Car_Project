b5089e1449ec599039800f2526ae30190ce48ee2
mahan.dev <mahansoodbakhsh@yahoo.com>
2025-10-12 22:53:25 +0330

    add images

--- app/components/modules/Categories.tsx
+++ app/components/modules/Categories.tsx
@@ -1,28 +1,43 @@
 import React from "react";
 
-import { CarIcon, CarModels } from "@/constants/Categories/Categories";
+import {
+  CarIcon,
+  CarImage,
+  CarModels,
+} from "@/constants/Categories/Categories";
 import { LetterUpperCase } from "@/app/helper/LetterUpperCase";
 
+import styles from "@/modules/styles/categories/categories.module.css";
+import CategoryIcon from "@/modules/CategoryIcon";
 
-
-import styles from "@/modules/styles/categories/banner.module.css";
-import CategoryIcon from "./CategoryIcon";
+// import SUVImage from "@/public/images/categories/suv.jpg";
+import Image from "next/image";
 
 type CartKey = keyof typeof CarIcon;
+type CarImageKey = keyof typeof CarImage;
 
 const Categories = () => {
   return (
     <section className={styles.container}>
       <ul>
         {CarModels.map((item) => (
-          <li className={styles.container__listItem} key={item}>
-            {item}
-            {CarIcon[LetterUpperCase(item) as CartKey] ? (
-              CarIcon[LetterUpperCase(item) as CartKey]
-            ) : (
-              <CategoryIcon item={item} />
-            )}
-          </li>
+          <div key={item} className={styles.container__card}>
+            <Image
+              sizes="100vw"
+              style={{ width: "100%", height: "110px" }}
+              src={CarImage[item.toLowerCase() as CarImageKey]}
+              alt={item}
+            />
+
+            <li className={styles.card__listItem} key={item}>
+              {item}
+              {CarIcon[LetterUpperCase(item) as CartKey] ? (
+                CarIcon[LetterUpperCase(item) as CartKey]
+              ) : (
+                <CategoryIcon item={item} />
+              )}
+            </li>
+          </div>
         ))}
       </ul>
     </section>
--- app/constants/Categories/Categories.tsx
+++ app/constants/Categories/Categories.tsx
@@ -3,7 +3,12 @@ import { FaCar } from "react-icons/fa";
 import { IoCarSportSharp } from "react-icons/io5";
 import { TbCarSuvFilled } from "react-icons/tb";
 
-
+import SuvImage from "@/public/images/categories/suv.jpg";
+import SedanImage from "@/public/images/categories/sedan.jpg";
+import CoupeImage from "@/public/images/categories/coupe.jpg";
+import HatchbackImage from "@/public/images/categories/hatchback.jpg";
+import ConvertibleImage from "@/public/images/categories/convertible.jpg";
+import SportImage from "@/public/images/categories/sport.jpg";
 
 export const CarIcon = {
   Suv: <TbCarSuvFilled />,
@@ -20,3 +25,12 @@ export const CarModels = [
   "CONVERTIBLE",
   "SPORT",
 ];
+
+export const CarImage = {
+  suv: SuvImage,
+  sedan: SedanImage,
+  coupe: CoupeImage,
+  hatchback: HatchbackImage,
+  convertible: ConvertibleImage,
+  sport: SportImage,
+};
